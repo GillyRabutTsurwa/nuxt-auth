5d17ff1794082ef53d942ad3bcb316d5be9fd5f1
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-08-01 15:44:37 -0400

    Built Sign In / Sign Out Functionality with Github Provider

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+const { status, signIn, signOut } = useAuth();
+
+async function handleSignIn(): Promise<void> {
+    await signIn();
+}
+
+async function handleSignOut(): Promise<void> {
+    await signOut();
+}
+
+const isLoggedIn: ComputedRef<boolean> = computed(() => status.value === "authenticated");
+
+</script>
+<template>
+    <div>
+        <button @click="handleSignOut" v-if="isLoggedIn">Sign Out</button>
+        <button @click="handleSignIn" v-else>Sign In</button>
+        <pre>{{ status }}</pre>
+    </div>
+</template>
