b40aed8f8c0f64f374d0cf40219bc2a7f601a0de
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-08-01 22:16:27 -0400

    Protected Pages - using the package middleware to create protected pages - it's as easy as calling the middleware inside defineMeta()

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  runtimeConfig: {
-    github: {
-      client_ID: process.env.GITHUB_CLIENT_ID,
-      client_secret: process.env.GITHUB_CLIENT_SECRET
-    }
-  },
-  modules: ["@sidebase/nuxt-auth"],
-  devtools: { enabled: true }
-})
+    runtimeConfig: {
+        github: {
+            client_ID: process.env.GITHUB_CLIENT_ID,
+            client_secret: process.env.GITHUB_CLIENT_SECRET,
+        },
+    },
+    modules: ["@sidebase/nuxt-auth"],
+    devtools: { enabled: true },
+});
---
+++ pages/private/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+/**
+ * NOTE: 
+ * i see. upon inspecting using the nuxt devtools,
+ * j'ai découvert que ce middleware vient du package @sidebase/nuxt-auth
+ * je comprenais pas parce que je savais pas d'ou il venait, et on a pas écrit de middleware
+ */
+definePageMeta({
+    middleware: "auth" // NOTE: encore une fois, ce middleware est une produit de @sidebase/nuxt-auth
+})
+</script>
+<template>
+    <div>
+        <h2>Protected Page. Authourised Users Only.</h2>
+    </div>
+</template>
+
+
+<style lang="scss" scoped></style>
